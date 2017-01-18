dd970f596902700eddaa4cd42e92d6234210171b
athan87 <athan@nxar.com>
2017-01-17 17:02:46 -0800

    manage rule page

--- assets/js/dependencies/rules-engine.js
+++ assets/js/dependencies/rules-engine.js
@@ -1,5 +1,24 @@
 jQuery(document).ready(function($) { 
-    $( "#actManageRules" ).click(function() {
-		$(".clBox_top").load('raw?view=timesheet/add_row_form');
+	$( "#actManageRules" ).click(function() {
+		$(".clBox_top").load('raw?view=rules-engine/manage_rules');
+	});
+
+	$(document).on('click', ".openModal", function(){
+		var mUrl = $(this).data("murl"); 
+		var mTitle = $(this).data("mtitle");
+		var mHandler = $(this).data("mhandler"); 
+		$("#modalBox .formMsgCont").empty();
+
+		$('#modalBox .modal-body').load(mUrl, "", function() {
+				$("#modalBox .modal-header h3").text(mTitle);
+				$("#modalBox #mdhandler").text(mHandler);
+				$("#modalBox .modal-footer").removeClass("success");
+				$("#modalBox #submitModal").show();
+				$("#modalBox").modal();
+			})
+	});
+
+	$("#actAddRules").click(function() {
+		$(".clBox_top").load('raw?view=rules-engine/manage_rules');		
 	});
 });
